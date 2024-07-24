2cf870710f1508a97bb22bdcc2f8518415e89f41
K1au3 <lindquistconcern@gmail.com>
2024-07-24 18:54:11 +0300

    less footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,68 @@ function toggleSubmenu(submenuId) {
     submenu.style.display = submenu.style.display === 'flex' ? 'none' : 'flex';
 }
 
+
+function toggleMenu() {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+}
+
+function toggleSubmenu(submenuId) {
+    const submenu = document.getElementById(submenuId);
+    submenu.style.display = submenu.style.display === 'flex' ? 'none' : 'flex';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.number');
+    counters.forEach(counter => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const increment = target / 200;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCounter, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+});
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../../reviews.json')
+        .then(response => response.json())
+        .then(data => {
+            const reviewsContainer = document.getElementById('reviewsContainer');
+            const reviewTrack = document.createElement('div');
+            reviewTrack.className = 'review-track';
+
+            data.reviews.forEach(review => {
+                const reviewCard = document.createElement('div');
+                reviewCard.className = 'review-card';
+                reviewCard.innerHTML = `
+                    <h2>${review.title}</h2>
+                    <p>${review.content}</p>
+                    <p class="name">${review.name}</p>
+                `;
+                reviewTrack.appendChild(reviewCard);
+            });
+
+            // Duplicate the reviewTrack for infinite scrolling effect
+            reviewTrack.innerHTML += reviewTrack.innerHTML;
+            reviewsContainer.appendChild(reviewTrack);
+        })
+        .catch(error => console.error('Error fetching reviews:', error));
+});
+
+
